50fb2b4c16142f6baf34c889bded437bf6120c43
Hrittik Chatterjee <hrittikchatterjee@gmail.com>
2021-02-24 14:29:34 -0800

    Started Raycasting fixes

--- index.js
+++ index.js
@@ -1,153 +1,232 @@
-var lesson10 = {
-    scene: null, camera: null, renderer: null,
-    container: null, controls: null,
-    clock: null, stats: null,
-    plane: null, selection: null, offset: new THREE.Vector3(), objects: [],
-    raycaster: new THREE.Raycaster(),
-    init: function() {
-      // Create main scene
-      this.scene = new THREE.Scene();
-      this.scene.fog = new THREE.FogExp2(0xcce0ff, 0.0003);
-      var SCREEN_WIDTH = window.innerWidth, SCREEN_HEIGHT = window.innerHeight;
-      // Prepare perspective camera
-      var VIEW_ANGLE = 45, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 1, FAR = 1000;
-      this.camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
-      this.scene.add(this.camera);
-      this.camera.position.set(100, 0, 0);
-      this.camera.lookAt(new THREE.Vector3(0,0,0));
-      // Prepare webgl renderer
-      this.renderer = new THREE.WebGLRenderer({ antialias:true });
-      this.renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
-      this.renderer.setClearColor(this.scene.fog.color);
-      // Prepare container
-      this.container = document.createElement('div');
-      document.body.appendChild(this.container);
-      this.container.appendChild(this.renderer.domElement);
-      // Events
-      THREEx.WindowResize(this.renderer, this.camera);
-      document.addEventListener('mousedown', this.onDocumentMouseDown, false);
-      document.addEventListener('mousemove', this.onDocumentMouseMove, false);
-      document.addEventListener('mouseup', this.onDocumentMouseUp, false);
-      // Prepare Orbit controls
-      this.controls = new THREE.OrbitControls(this.camera);
-      this.controls.target = new THREE.Vector3(0, 0, 0);
-      this.controls.maxDistance = 150;
-      // Prepare clock
-      this.clock = new THREE.Clock();
-      // Prepare stats
-      this.stats = new Stats();
-      this.stats.domElement.style.position = 'absolute';
-      this.stats.domElement.style.left = '50px';
-      this.stats.domElement.style.bottom = '50px';
-      this.stats.domElement.style.zIndex = 1;
-      this.container.appendChild( this.stats.domElement );
-      // Add lights
-      this.scene.add( new THREE.AmbientLight(0x444444));
-      var dirLight = new THREE.DirectionalLight(0xffffff);
-      dirLight.position.set(200, 200, 1000).normalize();
-      this.camera.add(dirLight);
-      this.camera.add(dirLight.target);
+import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r125/build/three.module.js';
+import {OBJLoader} from 'https://threejsfundamentals.org/threejs/resources/threejs/r125/examples/jsm/loaders/OBJLoader.js';
+import {OrbitControls} from './node_modules/three/examples/jsm/controls/OrbitControls.js';
+let isclicked = false
+const testRender = "./assets/TestRenderObj.obj"
+let clickTime
+function main() {
+  const canvas = document.getElementById("c");
+  const renderer = new THREE.WebGLRenderer({canvas});
+  const fov = 60;
+  const aspect = 2;  
+  const near = 0.1;
+  const far = 200;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  //camera.position.z = 60
+  camera.position.y = 60
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color('black');
+  const controls = new OrbitControls(camera, canvas);
+  controls.target.set(0, 5, 0);
+  controls.update();
+  {
+    const size = 50;
+    const divisions = 20;
+    const gridHelper = new THREE.GridHelper( size, divisions );
+    gridHelper.name = "Grid"
+    scene.add( gridHelper );
+  }
+  {
+  const loader = new OBJLoader();
+  loader.load(
+    'assets/TestRenderObj.obj',
+    function ( object ) {
+      scene.add( object );
     },
-    addSkybox: function() {
+    function ( xhr ) {
+      console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
     },
-    onDocumentMouseDown: function (event) {
-    },
-    onDocumentMouseMove: function (event) {
-    },
-    onDocumentMouseUp: function (event) {
+    function ( error ) {
+      console.log( 'An error happened' );
     }
-  };
-  // Animate the scene
-  function animate() {
-    requestAnimationFrame(animate);
-    render();
-    update();
+  );
   }
-  // Update controls and stats
-  function update() {
-    var delta = lesson10.clock.getDelta();
-    lesson10.controls.update(delta);
-    lesson10.stats.update();
+  {
+    const boxWidth = 1;
+    const boxHeight = 1;
+    const boxDepth = 1;
+    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+    const cube = new THREE.Mesh(geometry);
+    cube.material.color.set(Math.random() + 0xEEEEEE)
+
+    scene.add(cube);
   }
-  // Render the scene
-  function render() {
-    if (lesson10.renderer) {
-      lesson10.renderer.render(lesson10.scene, lesson10.camera);
+  const cameraPole = new THREE.Object3D();
+  scene.add(cameraPole);
+  cameraPole.add(camera);
+
+  {
+    const color = 0xFFFFFF;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+    camera.add(light);
+  }
+
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  }
+
+  class PickHelper {
+    constructor() {
+      this.raycaster = new THREE.Raycaster();
+      this.pickedObject = null;
+      this.pickedObjectSavedColor = null;
+    }
+    pick(normalizedPosition, scene, camera, time) {
+      if(isclicked == true){
+      this.raycaster.setFromCamera(normalizedPosition, camera);
+      const intersectedObjects = this.raycaster.intersectObjects(scene.children);
+      if (intersectedObjects.length != 0) {
+        let Objname = intersectedObjects[0].object.name
+        if(Objname == "Arrow1" || Objname == "Arrow2" || Objname == "Arrow3" || Objname == "Arrow4" || Objname == "Arrow5"){
+          console.log(" ")
+        }
+        if(intersectedObjects[0].object.name == "Grid" || intersectedObjects[0].object.name == "GridHelper"){
+          if(this.pickedObject != null && this.pickedObject != undefined){
+            this.pickedObject.material.color.set(Math.random() + 0xEEEEEE)
+            this.pickedObjectSavedColor = 0
+            this.pickedObject = undefined
+            arrowHandling("remove")
+            isclicked = false
+          }
+        }else{
+          if(this.pickedObjectSavedColor == null || this.pickedObjectSavedColor == 0){
+            this.pickedObject = intersectedObjects[0].object;
+            this.pickedObjectSavedColor = intersectedObjects[0].object.material.color
+            arrowHandling("add")
+            isclicked = false
+          }
+          this.pickedObject.material.color.set(Math.random() + 0x0FFF00)
+        }
+      }else{
+        if(this.pickedObject && this.pickedObjectSavedColor){
+          this.pickedObject.material.color.set(Math.random() + 0xEEEEEE)
+          this.pickedObjectSavedColor = 0
+          this.pickedObject = undefined
+          arrowHandling("remove")
+          isclicked = false
+
+        }
+      }
+    }
     }
   }
-  // Initialize lesson on page load
-  function initializeLesson() {
-    lesson10.init();
-    animate();
-  }
-  if (window.addEventListener)
-    window.addEventListener('load', initializeLesson, false);
-  else if (window.attachEvent)
-    window.attachEvent('onload', initializeLesson);
-  else window.onload = initializeLesson;
-  
-  sbVertexShader = [
-    "varying vec3 vWorldPosition;",
-    "void main() {",
-    "  vec4 worldPosition = modelMatrix * vec4( position, 1.0 );",
-    "  vWorldPosition = worldPosition.xyz;",
-    "  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
-    "}",
-    ].join("\n");
-    sbFragmentShader = [
-    "uniform vec3 topColor;",
-    "uniform vec3 bottomColor;",
-    "uniform float offset;",
-    "uniform float exponent;",
-    "varying vec3 vWorldPosition;",
-    "void main() {",
-    "  float h = normalize( vWorldPosition + offset ).y;",
-    "  gl_FragColor = vec4( mix( bottomColor, topColor, max( pow( h, exponent ), 0.0 ) ), 1.0 );",
-    "}",
-    ].join("\n");
-    // Add 100 random objects (spheres)
-  var object, material, radius;
-  var objGeometry = new THREE.SphereGeometry(1, 24, 24);
-  for (var i = 0; i < 50; i++) {
-    material = new THREE.MeshPhongMaterial({color: Math.random() * 0xffffff});
-    material.transparent = true;
-    object = new THREE.Mesh(objGeometry.clone(), material);
-    this.objects.push(object);
-    radius = Math.random() * 4 + 2;
-    object.scale.x = radius;
-    object.scale.y = radius;
-    object.scale.z = radius;
-    object.position.x = Math.random() * 50 - 25;
-    object.position.y = Math.random() * 50 - 25;
-    object.position.z = Math.random() * 50 - 25;
-    this.scene.add(object);
-}
-// Plane, that helps to determinate an intersection position
-this.plane = new THREE.Mesh(new THREE.PlaneBufferGeometry(500, 500, 8, 8), new THREE.MeshBasicMaterial({color: 0xffffff}));
-this.plane.visible = false;
-this.scene.add(this.plane);
+ 
+  const pickPosition = {x: 0, y: 0};
+  const pickHelper = new PickHelper();
+  clearPickPosition();
 
-onDocumentMouseDown: function (event) {
-  // Get mouse position
-  var mouseX = (event.clientX / window.innerWidth) * 2 - 1;
-  var mouseY = -(event.clientY / window.innerHeight) * 2 + 1;
-  // Get 3D vector from 3D mouse position using 'unproject' function
-  var vector = new THREE.Vector3(mouseX, mouseY, 1);
-  vector.unproject(lesson10.camera);
-  // Set the raycaster position
-  lesson10.raycaster.set( lesson10.camera.position, vector.sub( lesson10.camera.position ).normalize() );
-  // Find all intersected objects
-  var intersects = lesson10.raycaster.intersectObjects(lesson10.objects);
-  if (intersects.length > 0) {
-    // Disable the controls
-    lesson10.controls.enabled = false;
-    // Set the selection - first intersected object
-    lesson10.selection = intersects[0].object;
-    // Calculate the offset
-    var intersects = lesson10.raycaster.intersectObject(lesson10.plane);
-    lesson10.offset.copy(intersects[0].point).sub(lesson10.plane.position);
+  function render(time) {
+    time *= 1;
+
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+    pickHelper.pick(pickPosition, scene, camera, time);
+
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
   }
+  requestAnimationFrame(render);
+
+  function getCanvasRelativePosition(event) {
+    const rect = canvas.getBoundingClientRect();
+    return {
+      x: (event.clientX - rect.left) * canvas.width  / rect.width,
+      y: (event.clientY - rect.top ) * canvas.height / rect.height,
+    };
+  }
+
+  function setPickPosition(event) {
+    let today = new Date();
+    let mili1 = today.getMilliseconds()
+    let mili2 = clickTime.getMilliseconds()
+    let mili = mili1 - mili2
+    if(mili < 200 && mili > 0){
+      console.log(mili)
+      isclicked = true
+      const pos = getCanvasRelativePosition(event);
+      pickPosition.x = (pos.x / canvas.width ) *  2 - 1;
+      pickPosition.y = (pos.y / canvas.height) * -2 + 1;
+    }else{
+      
+    }
+    
 }
 
-
+  function clearPickPosition() {
+    pickPosition.x = -100000;
+    pickPosition.y = -100000;
+  }
+  function onMouseDown(){
+    let today = new Date();
+    clickTime = today
+  }
+  canvas.addEventListener("mousedown", onMouseDown);
+  canvas.addEventListener("mouseup", setPickPosition)
+  //canvas.addEventListener("click", setPickPosition)
   
+  // let c = document.getElementById("c")
+  // console.log(c)
+  // c.addEventListener('mouseup', setPickPosition);
+  // c.addEventListener('mousedown', mousedownfunc);
+  // function mousedownfunc(){
+  //   console.log("tesjl")
+  //   var date = new Date();
+  //   clickTime = date.getSeconds();
+  // }
+  function arrowHandling(action){
+    var i;
+    let dir = new THREE.Vector3(-90, 0, 0 );
+    dir.normalize();
+    const origin = new THREE.Vector3( 0, 0, 0 );
+    const length = 3;
+    const hex = 0xffff00;
+    if(action == "add"){
+      for(i = 0; i <= 6; i++){
+        const arrow = new THREE.ArrowHelper( dir, origin, length, hex );
+        arrow.name = "Arrow" + i.toString()
+        if(i < 2){
+          dir.x += 90
+        }else if(i < 4){
+          dir.x = 0
+          if(i == 3){
+            dir.y = 90
+          }else{
+            dir.y = -90
+          }
+        }else if(i < 6){
+          dir.y = 0
+          if(i == 4){
+            dir.z = 90
+          }else{
+            dir.z = -90
+            console.log(scene)
+          }
+        }
+        scene.add( arrow );
+      }
+    }else{
+      for(i = 0; i <= 6; i++){
+        let arrow = scene.getObjectByName("Arrow" + i.toString());
+        if(arrow){
+          scene.remove( arrow );
+        }
+      }
+    }
+  }
+  
+}
+main(); 
